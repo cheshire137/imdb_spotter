517d668170ae8e1500aaff001b400a690ee83375
Sarah Vessels <cheshire137@gmail.com>
2017-02-04 14:32:15 -0600

    Update copyright year in source

--- extension/popup.js
+++ extension/popup.js
@@ -1,5 +1,5 @@
 /*
- * Copyright 2013 Sarah Vessels
+ * Copyright 2017 Sarah Vessels
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
