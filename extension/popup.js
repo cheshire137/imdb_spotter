fc60fb2f34e78ea88b10f7ea547105acdcc02c21
Sarah Vessels <cheshire137@gmail.com>
2017-02-04 11:14:25 -0600

    Move Google Analytics into html

--- extension/popup.js
+++ extension/popup.js
@@ -15,20 +15,6 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
-var _gaq = _gaq || []
-_gaq.push(['_setAccount', 'UA-42851748-1'])
-_gaq.push(['_trackPageview'])
-
-(function() {
-  const ga = document.createElement('script')
-  ga.type = 'text/javascript'
-  ga.async = true
-  ga.src = 'https://ssl.google-analytics.com/ga.js'
-
-  const s = document.getElementsByTagName('script')[0]
-  s.parentNode.insertBefore(ga, s)
-})();
-
 const imdbSpotterPopup = {
   getSpotifyTrackSearchUrl: function(query) {
     return `http://ws.spotify.com/search/1/track.json?q=${encodeURIComponent(query)}`
